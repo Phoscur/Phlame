bcbe0a3c896c209eef410c3f3a83096147cdc641
Phoscur <phoscur@pheelgood.net>
2020-04-26 22:53:52 +0200

    Add resource types and amount underflow handling

--- src/resources/Resource.spec.ts
+++ src/resources/Resource.spec.ts
@@ -4,6 +4,7 @@ import Resource from "./Resource";
 
 describe("Resource ValueObject", () => {
   it("should be console printable", () => {
+    Resource.types.push("irrelevant");
     const resource = new Resource("irrelevant", 0);
     expect(resource.toString()).to.eql("Resource[0irrelevant]");
   });
--- src/resources/Resource.ts
+++ src/resources/Resource.ts
@@ -1,12 +1,17 @@
 
 export default class Resource {
+  static types = ["null"];
+
   readonly type: string;
 
-  readonly amount: number;
+  readonly amount: number; // int32
 
   constructor(type: string, amount: number) {
-    this.amount = amount;
-    this.type = type;
+    // Instead of throwing an error, set resource amount to zero on underflow
+    // TODO handle overflow? use BigIntegers?
+    this.amount = amount < 0 ? 0 : amount | 0; // int32|0 handling is very fast in v8
+    // Default to null type (!~ = not found)
+    this.type = !~Resource.types.indexOf(type) ? Resource.types[0] : type;
   }
 
   get prettyAmount(): string {
