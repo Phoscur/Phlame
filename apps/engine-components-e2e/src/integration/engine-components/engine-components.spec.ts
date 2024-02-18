41749b09e8cab1661989cb263e59e141ad0a6952
Phoscur <phoscur@pheelgood.net>
2024-02-18 18:30:28 +0100

    Remove app, engine-components, playground(-e2e), console & .storybook

--- apps/engine-components-e2e/src/integration/engine-components/engine-components.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('engine-components: EngineComponents component', () => {
-
-  beforeEach(() => cy.visit('/iframe.html?id=enginecomponents--primary&knob-resources=engine-components&theme-ind=0&theme-sidebar=true&theme-full=true'));
-
-  it('should render the component', () => {
-    cy.get('h1').should('contain', 'here be engine-components!');
-  });
-});
