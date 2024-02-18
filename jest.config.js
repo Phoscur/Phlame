72e5e0c7aa0899dc8136d7c8f3baaf428ffede0f
Phoscur <phoscur@pheelgood.net>
2024-02-18 18:30:28 +0100

    Remove app, engine-components, playground(-e2e), console & .storybook

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   projects: [
-    "<rootDir>/apps/console",
     "<rootDir>/libs/engine",
-    "<rootDir>/apps/playground",
-    "<rootDir>/apps/server",
-    "<rootDir>/apps/app",
-    "<rootDir>/libs/engine-components",
   ],
 };
--- libs/engine-components/README.md
+++
@@ -1,7 +0,0 @@
-# engine-components
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test engine-components` to execute the unit tests via [Jest](https://jestjs.io).
