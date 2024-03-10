0c927652182c1883142067605e5e4b0aac84f65f
Phoscur <phoscur@pheelgood.net>
2024-03-10 23:25:51 +0100

    Generate some haikei icons and add them as tsx

---
+++ src/app/icons.svg.tsx
@@ -0,0 +1,174 @@
+export const MetallicIcon = () => (
+  <>
+    <svg
+      viewBox="0 0 90 90"
+      width="90"
+      height="90"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+    >
+      <g stroke-width="0.1" stroke-linejoin="bevel">
+        <path d="M47 46L38 60L53 50Z" fill="#211e1c" stroke="#a8a29e"></path>
+        <path d="M38 60L54 60L53 50Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M54 60L61 55L53 50Z" fill="#41403f" stroke="#a8a29e"></path>
+        <path d="M57 29L44 36L47 46Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M62 32L47 46L53 50Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M47 46L44 36L38 60Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M38 60L40 74L54 60Z" fill="#3b3a39" stroke="#a8a29e"></path>
+        <path d="M54 60L65 61L61 55Z" fill="#41403f" stroke="#a8a29e"></path>
+        <path d="M59 77L65 61L54 60Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M59 77L70 60L65 61Z" fill="#41403f" stroke="#a8a29e"></path>
+        <path d="M65 61L70 60L61 55Z" fill="#3b3a39" stroke="#a8a29e"></path>
+        <path d="M61 55L62 32L53 50Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M40 74L59 77L54 60Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M68 32L62 32L61 55Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M62 32L57 29L47 46Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M30 28L17 66L38 60Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M38 60L17 66L40 74Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M44 13L30 28L44 36Z" fill="#2b2927" stroke="#a8a29e"></path>
+        <path d="M44 36L30 28L38 60Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M62 32L68 32L57 29Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M70 60L68 32L61 55Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M40 74L59 90L59 77Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M59 77L87 82L70 60Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M45 98L59 90L40 74Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M59 90L67 90L59 77Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M57 29L44 13L44 36Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M50 11L44 13L57 29Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M67 90L69 90L59 77Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M26 103L45 98L40 74Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M59 90L45 98L67 90Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M67 90L45 98L69 90Z" fill="#41403f" stroke="#a8a29e"></path>
+        <path d="M26 0L13 26L30 28Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M30 28L1 50L17 66Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M60 0L50 11L57 29Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M13 26L1 50L30 28Z" fill="#2b2927" stroke="#a8a29e"></path>
+        <path d="M17 66L26 103L40 74Z" fill="#2b2927" stroke="#a8a29e"></path>
+        <path d="M87 82L90 59L70 60Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M70 60L90 40L68 32Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M1 50L0 53L17 66Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M13 26L0 46L1 50Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M1 50L0 46L0 53Z" fill="#211e1c" stroke="#a8a29e"></path>
+        <path d="M90 59L90 40L70 60Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M68 32L60 0L57 29Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M90 40L83 25L68 32Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M0 53L0 73L17 66Z" fill="#41403f" stroke="#a8a29e"></path>
+        <path d="M90 59L94 62L90 40Z" fill="#3b3a39" stroke="#a8a29e"></path>
+        <path d="M87 82L94 62L90 59Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M69 90L87 82L59 77Z" fill="#3b3a39" stroke="#a8a29e"></path>
+        <path d="M0 24L0 30L13 26Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M13 26L0 30L0 46Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M3 90L26 103L17 66Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M90 40L90 26L83 25Z" fill="#211e1c" stroke="#a8a29e"></path>
+        <path d="M60 0L52 0L50 11Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M94 62L90 26L90 40Z" fill="#3b3a39" stroke="#a8a29e"></path>
+        <path d="M16 0L0 24L13 26Z" fill="#211e1c" stroke="#a8a29e"></path>
+        <path d="M26 0L30 28L44 13Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M0 73L3 90L17 66Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M60 0L68 32L83 25Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M50 11L37 0L44 13Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M0 53L-10 56L0 73Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M0 73L2 90L3 90Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M0 46L-10 56L0 53Z" fill="#363433" stroke="#a8a29e"></path>
+        <path d="M0 30L-10 56L0 46Z" fill="#41403f" stroke="#a8a29e"></path>
+        <path d="M0 24L-10 56L0 30Z" fill="#3b3a39" stroke="#a8a29e"></path>
+        <path d="M52 0L37 0L50 11Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M0 90L2 90L0 73Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M3 90L2 90L26 103Z" fill="#474645" stroke="#a8a29e"></path>
+        <path d="M94 62L90 21L90 26Z" fill="#2b2927" stroke="#a8a29e"></path>
+        <path d="M90 26L90 21L83 25Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M90 96L90 90L69 90Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M69 90L90 90L87 82Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M87 82L90 90L94 62Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M90 96L69 90L45 98Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M90 0L60 0L83 25Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M-10 56L0 90L0 73Z" fill="#312e2d" stroke="#a8a29e"></path>
+        <path d="M2 90L0 90L26 103Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M37 0L26 0L44 13Z" fill="#211e1c" stroke="#a8a29e"></path>
+        <path d="M26 103L90 96L45 98Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+        <path d="M90 90L90 96L94 62Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M26 0L16 0L13 26Z" fill="#262322" stroke="#a8a29e"></path>
+        <path d="M94 62L90 0L90 21Z" fill="#525252" stroke="#a8a29e"></path>
+        <path d="M90 21L90 0L83 25Z" fill="#2b2927" stroke="#a8a29e"></path>
+        <path d="M16 0L0 0L0 24Z" fill="#1c1917" stroke="#a8a29e"></path>
+        <path d="M0 24L0 0L-10 56Z" fill="#4c4c4c" stroke="#a8a29e"></path>
+      </g>
+    </svg>
+  </>
+);
+export const CrystallineIcon = () => (
+  <>
+    <svg
+      viewBox="0 0 90 90"
+      width="90"
+      height="90"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+    >
+      <g stroke-width="0.3" stroke-linejoin="bevel">
+        <path d="M47.5 19.5L51.5 0L38 0Z" fill="#868698" stroke="#4b5563"></path>
+        <path d="M38 0L24 6.5L47.5 19.5Z" fill="#515375" stroke="#4b5563"></path>
+        <path d="M38 0L18.5 0L24 6.5Z" fill="#868698" stroke="#4b5563"></path>
+        <path d="M90 0L63.5 -12.5L51.5 0Z" fill="#6b6c87" stroke="#4b5563"></path>
+        <path d="M51.5 0L63.5 -12.5L38 0Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M38 0L63.5 -12.5L18.5 0Z" fill="#a1a1aa" stroke="#4b5563"></path>
+        <path d="M18.5 0L0 17L24 6.5Z" fill="#6b6c87" stroke="#4b5563"></path>
+        <path d="M24 6.5L0 55.5L47.5 19.5Z" fill="#868698" stroke="#4b5563"></path>
+        <path d="M0 -2L0 0L18.5 0Z" fill="#515375" stroke="#4b5563"></path>
+        <path d="M18.5 0L0 0L0 17Z" fill="#78798f" stroke="#4b5563"></path>
+        <path d="M47.5 19.5L90 16L51.5 0Z" fill="#43476d" stroke="#4b5563"></path>
+        <path d="M47.5 19.5L90 16.5L90 16Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M-8.5 -10L0 -2L18.5 0Z" fill="#78798f" stroke="#4b5563"></path>
+        <path d="M90 16L90 0L51.5 0Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M90 0L90 -0.5L63.5 -12.5Z" fill="#9393a1" stroke="#4b5563"></path>
+        <path d="M0 0L-8.5 12.5L0 17Z" fill="#27305c" stroke="#4b5563"></path>
+        <path d="M-10 7.5L-8.5 12.5L0 0Z" fill="#363b65" stroke="#4b5563"></path>
+        <path d="M0 -2L-10 7.5L0 0Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M63.5 -12.5L-8.5 -10L18.5 0Z" fill="#a1a1aa" stroke="#4b5563"></path>
+        <path d="M0 -2L-8.5 -10L-10 7.5Z" fill="#78798f" stroke="#4b5563"></path>
+        <path d="M0 90L0 55.5L-8.5 12.5Z" fill="#a1a1aa" stroke="#4b5563"></path>
+        <path d="M-8.5 12.5L0 55.5L0 17Z" fill="#5e5f7e" stroke="#4b5563"></path>
+        <path d="M0 17L0 55.5L24 6.5Z" fill="#868698" stroke="#4b5563"></path>
+        <path d="M47.5 19.5L96.5 74.5L90 16.5Z" fill="#6b6c87" stroke="#4b5563"></path>
+        <path d="M90 90L96.5 74.5L47.5 19.5Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M90 16.5L96.5 74.5L90 16Z" fill="#515375" stroke="#4b5563"></path>
+        <path d="M90 16L96.5 74.5L90 0Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M90 0L96.5 74.5L90 -0.5Z" fill="#9393a1" stroke="#4b5563"></path>
+        <path d="M0 90L3.5 90L0 55.5Z" fill="#6b6c87" stroke="#4b5563"></path>
+        <path d="M17.5 102L90 90L47.5 19.5Z" fill="#27305c" stroke="#4b5563"></path>
+        <path d="M-10 7.5L0 90L-8.5 12.5Z" fill="#78798f" stroke="#4b5563"></path>
+        <path d="M17.5 102L47.5 19.5L0 55.5Z" fill="#363b65" stroke="#4b5563"></path>
+        <path d="M90 90L94 90L96.5 74.5Z" fill="#9393a1" stroke="#4b5563"></path>
+        <path d="M94 90L94.5 90L96.5 74.5Z" fill="#43476d" stroke="#4b5563"></path>
+        <path d="M0 90L0 94.5L3.5 90Z" fill="#172554" stroke="#4b5563"></path>
+        <path d="M3.5 90L17.5 102L0 55.5Z" fill="#6b6c87" stroke="#4b5563"></path>
+        <path d="M90 90L17.5 102L94 90Z" fill="#9393a1" stroke="#4b5563"></path>
+        <path d="M94 90L17.5 102L94.5 90Z" fill="#43476d" stroke="#4b5563"></path>
+        <path d="M-10 7.5L0 94.5L0 90Z" fill="#868698" stroke="#4b5563"></path>
+        <path d="M0 94.5L17.5 102L3.5 90Z" fill="#9393a1" stroke="#4b5563"></path>
+      </g>
+    </svg>
+  </>
+);
+export const BubblesIcon = () => (
+  <>
+    <svg
+      viewBox="0 0 90 90"
+      width="90"
+      height="90"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+    >
+      <g fill="#3b82f6">
+        <circle r="19" cx="51" cy="67"></circle>
+        <circle r="8" cx="47" cy="10"></circle>
+        <circle r="15" cx="12" cy="45"></circle>
+        <circle r="16" cx="78" cy="29"></circle>
+        <circle r="17" cx="12" cy="82"></circle>
+      </g>
+    </svg>
+  </>
+);
