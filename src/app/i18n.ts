bd8048dcc98d3f84f8cc320b0735d98e7912a7df
Phoscur <phoscur@pheelgood.net>
2024-03-13 23:24:16 +0100

    Add i18n

---
+++ src/app/i18n.ts
@@ -0,0 +1,35 @@
+export const languages = {
+  en: 'English',
+  de: 'Deutsch',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.planet': 'Planet',
+    'resource.metallic': 'Metals',
+    'resource.crystalline': 'Crystals',
+    'resource.bubbles': 'Bubbles',
+  },
+  de: {
+    'nav.home': 'Übersicht',
+  },
+} as const;
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export type TranslationIndex = (typeof ui)[typeof defaultLang];
+export type TranslationEntries = keyof TranslationIndex;
+export type I18n = (key: TranslationEntries) => string;
+
+export function useTranslations(lang: keyof typeof ui): I18n {
+  return function t(key) {
+    return ((ui[lang] || ui[defaultLang]) as TranslationIndex)[key];
+  };
+}
