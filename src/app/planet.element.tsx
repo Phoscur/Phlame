1a1ce92e7726982758b745f390a8e29068c74255
Phoscur <phoscur@pheelgood.net>
2024-03-23 18:42:58 +0100

    Add tailwind breakpoints/colors

--- src/app/planet.element.tsx
+++ src/app/planet.element.tsx
@@ -4,16 +4,16 @@ import { BubblesIcon, CrystallineIcon, MetallicIcon } from './icons.svg';
 
 const template = (t: I18n) => (
   <>
-    <h2 class="text-xl font-semibold mb-2">Resource 1</h2>
+    <h2 class="text-xl font-semibold mb-2">Planet</h2>
     <img src="/dall-e-planet.png" alt="Planet" class="w-20 mx-auto mb-4" />
     <ul>
-      <li>
+      <li class="bg-neon-pink">
         <MetallicIcon /> {t('resource.metallic')}- 1000
       </li>
-      <li>
+      <li class="bg-neon-lime">
         <CrystallineIcon /> {t('resource.crystalline')}- 1000
       </li>
-      <li>
+      <li class="bg-neon-cyan">
         <BubblesIcon /> {t('resource.bubbles')}- 1000
       </li>
     </ul>
