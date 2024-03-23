d2f4a5326ff9836f7b0d951f7e809bc7d4615f96
Phoscur <phoscur@pheelgood.net>
2024-03-23 23:39:48 +0100

    Add icon colors to theme css

--- src/app/planet.element.tsx
+++ src/app/planet.element.tsx
@@ -7,13 +7,13 @@ const template = (t: I18n) => (
     <h2 class="text-xl font-semibold mb-2">Planet</h2>
     <img src="/dall-e-planet.png" alt="Planet" class="w-20 mx-auto mb-4" />
     <ul>
-      <li class="bg-neon-pink">
+      <li class="bg-m-f1">
         <MetallicIcon /> {t('resource.metallic')}- 1000
       </li>
-      <li class="bg-neon-lime">
+      <li class="bg-c-f1">
         <CrystallineIcon /> {t('resource.crystalline')}- 1000
       </li>
-      <li class="bg-neon-cyan">
+      <li class="bg-b-f2">
         <BubblesIcon /> {t('resource.bubbles')}- 1000
       </li>
     </ul>
