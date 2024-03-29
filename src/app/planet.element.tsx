e2946a16f90a6fa83277674eb6f549d926f1d39b
Phoscur <phoscur@pheelgood.net>
2024-03-29 09:41:05 +0100

    Add resource pills, set planet image as card background, clean up colors

--- src/app/planet.element.tsx
+++ src/app/planet.element.tsx
@@ -4,19 +4,74 @@ import { BubblesIcon, CrystallineIcon, MetallicIcon } from './icons.svg';
 
 const template = (t: I18n) => (
   <>
-    <h2 class="text-xl font-semibold mb-2">Planet</h2>
-    <img src="/dall-e-planet.png" alt="Planet" class="w-20 mx-auto mb-4" />
-    <ul>
-      <li class="bg-m-f1">
-        <MetallicIcon /> {t('resource.metallic')}- 1000
-      </li>
-      <li class="bg-c-f1">
-        <CrystallineIcon /> {t('resource.crystalline')}- 1000
-      </li>
-      <li class="bg-b-f2">
-        <BubblesIcon /> {t('resource.bubbles')}- 1000
-      </li>
-    </ul>
+    <div class="bg-gray-800 p-4 rounded-lg bg-cover bg-[url('/dall-e-planet.png')]">
+      <div class="flex justify-between">
+        <div class="min-w-0">
+          <h2 class="text-2xl font-bold leading-7 text-gray-500 sm:truncate sm:text-3xl sm:tracking-tight">
+            Planet
+          </h2>
+          <div>
+            <div class="mt-2 flex items-center text-sm text-gray-500">
+              <svg
+                class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              Galaxy
+            </div>
+          </div>
+        </div>
+        <div class="mt-5 flex lg:ml-4 lg:mt-0">
+          <span class="">
+            <span
+              class="bg-gray-700 text-gray-400 inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold
+              shadow-sm ring-1 ring-inset ring-gray-800 hover:bg-gray-600"
+            >
+              <MetallicIcon className="-ml-0.5 mr-1.5 h-5 w-5" />
+              1000
+            </span>
+          </span>
+          <span class="ml-2">
+            <span
+              class="bg-red-950 text-red-400 inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold
+              shadow-sm ring-1 ring-inset ring-red-800 hover:bg-red-800"
+            >
+              <CrystallineIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-red-950" />
+              1000
+            </span>
+          </span>
+          <span class="ml-2">
+            <span
+              class="bg-blue-950 text-blue-500 inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold
+              shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-blue-400"
+            >
+              <BubblesIcon className="-ml-0.5 mr-1.5 h-5 w-5" />
+              1000
+            </span>
+          </span>
+        </div>
+      </div>
+      <br class="mb-[200px]" />
+      <ul>
+        <li class="bg-metallic-dark text-metallic">
+          <MetallicIcon /> {t('resource.metallic')} - 1000
+        </li>
+        <li class="bg-crystalline-dark text-crystalline">
+          <CrystallineIcon className="text-crystalline-dark" />
+          {t('resource.crystalline')} - 9999
+        </li>
+        <li class="bg-liquid-dark text-liquid">
+          <BubblesIcon /> {t('resource.bubbles')} - 1000
+        </li>
+      </ul>
+    </div>
   </>
 );
 
