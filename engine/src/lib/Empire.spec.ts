a30ca650dbdf2d3c6d3b3051a9138963cd4b0475
Phoscur <phoscur@pheelgood.net>
2024-05-05 22:16:32 +0200

    Add Phlame Entity to as child of the Empire Entity

--- engine/src/lib/Empire.spec.ts
+++ engine/src/lib/Empire.spec.ts
@@ -1,26 +1,24 @@
-import examples, { Types } from './resources/examples';
 import ResourceCollection from './resources/ResourceCollection';
 import Stock from './resources/Stock';
-import Building, { BuildingIdentifier } from './Building';
+import type { BuildingIdentifier } from './Building';
+import Phlame from './Phlame';
 import Empire from './Empire';
+import examples, { Types } from './resources/examples';
+import { buildings } from './examples';
 
 describe('Empire Entity', () => {
   it('should be console printable', () => {
-    const { t3, s3 } = examples;
-    const stock = new Stock<Types>(ResourceCollection.fromArray([t3, s3]));
-    const empire = new Empire('Empire', stock);
-    expect(empire.toString()).to.eql(
-      'Empire (Stock[3tumbles(0, Infinity), 3salties(0, Infinity)]) []',
-    );
+    const empire = new Empire('Empire', []);
+    expect(empire.toString()).to.eql('Empire []');
   });
 
   it('should have resources and buildings', () => {
     const { t3, s3 } = examples;
-    const buildings: Building<BuildingIdentifier, Types>[] = [];
     const stock = new Stock<Types>(ResourceCollection.fromArray([t3, s3]));
-    const empire = new Empire('AllFresh', stock, buildings);
+    const phlame = new Phlame<Types, BuildingIdentifier>('Phlame', stock, buildings);
+    const empire = new Empire('Empirial', [phlame]);
     expect(empire.toString()).to.eql(
-      'AllFresh (Stock[3tumbles(0, Infinity), 3salties(0, Infinity)]) []',
+      'Empirial [Phlame (Stock[3tumbles(0, Infinity), 3salties(0, Infinity)]) Building(12, 1, 100%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 1, 100%)]',
     );
   });
 });
