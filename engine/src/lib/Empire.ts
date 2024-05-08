9731f73e640d69e54a372cfb9e36edd76da1f7bd
Phoscur <phoscur@pheelgood.net>
2024-05-08 22:16:07 +0200

    Add Empire implements Entity interface

--- engine/src/lib/Empire.ts
+++ engine/src/lib/Empire.ts
@@ -1,12 +1,13 @@
 import type { ResourceIdentifier } from './resources';
 import type { BuildingIdentifier } from './Building';
 import Phlame from './Phlame';
-import { ID } from './Action';
+import { Entity, ID } from './Action';
 
 export default class Empire<
   ResourceType extends ResourceIdentifier,
   BuildingType extends BuildingIdentifier,
-> {
+> implements Entity
+{
   constructor(public id: ID, public entities: Phlame<ResourceType, BuildingType>[]) {}
 
   toString(): string {
