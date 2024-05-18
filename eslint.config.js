dfd790df0ac134552dfab91b5816b7b73ca9e286
Phoscur <phoscur@pheelgood.net>
2024-05-18 21:15:06 +0200

    Add eslint-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,26 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import vitest from "eslint-plugin-vitest";
-import playwright from 'eslint-plugin-playwright'
-
+import vitest from 'eslint-plugin-vitest';
+import playwright from 'eslint-plugin-playwright';
 
 export default tseslint.config(
-  { ignores: ["playwright-report/*", "assets/*", "dist/*"]},
-  eslint.configs.recommended, /* eslint-disable-line @typescript-eslint/no-unsafe-argument *//* eslint-disable-line @typescript-eslint/no-unsafe-member-access */
+  { ignores: ['playwright-report/*', 'assets/*', 'dist/*'] },
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-argument */ /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */
+  eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   ...tseslint.configs.strictTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        project: ['./tsconfig.eslint.json', './engine/tsconfig.eslint.json', './tsconfig.spec.json', './engine/tsconfig.spec.json', './tsconfig.vite.json'],
+        project: [
+          './tsconfig.eslint.json',
+          './engine/tsconfig.eslint.json',
+          './tsconfig.spec.json',
+          './engine/tsconfig.spec.json',
+          './tsconfig.vite.json',
+        ],
         tsconfigRootDir: import.meta.dirname,
       },
     },
@@ -26,31 +32,31 @@ export default tseslint.config(
     },
   },
   {
-      files: ["**/*.spec.ts", "**/vite.config.ts"], // or any other pattern
-      plugins: {
-        vitest
+    files: ['**/*.spec.ts', '**/vite.config.ts'], // or any other pattern
+    plugins: {
+      vitest,
+    },
+    settings: {
+      vitest: {
+        typecheck: true,
       },
-      settings: {
-        vitest: {
-          typecheck: true,
-        }
-      },  
-      languageOptions: {
-        globals: {
-          ...vitest.environments.env.globals,
-        },
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
       },
-      rules: {
-        ...vitest.configs.recommended.rules,
-        //...vitest.configs.all.rules,
-        "vitest/max-nested-describe": ["error", { "max": 3 }],
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        'vitest/valid-expect': 'off', // TODO? refactor matchers?
-    }, 
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      //...vitest.configs.all.rules,
+      'vitest/max-nested-describe': ['error', { max: 3 }],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      'vitest/valid-expect': 'off', // TODO? refactor matchers?
+    },
   },
-  
+
   {
     ...playwright.configs['flat/recommended'],
     files: ['tests/**'],
