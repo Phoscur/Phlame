6c23695d4dc52fa179b2cc05639d2afeadfadacf
Phoscur <phoscur@pheelgood.net>
2024-05-18 18:40:36 +0200

    Add eslint-plugin-playwright

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,55 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettierConfig from 'eslint-config-prettier';
+import vitest from "eslint-plugin-vitest";
+import playwright from 'eslint-plugin-playwright'
+
 
 export default tseslint.config(
+  { ignores: ["playwright-report/*", "assets/*", "dist/*"]},
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-   //...tseslint.configs.strictTypeChecked,
-   //...tseslint.configs.stylisticTypeChecked,
-   ...tseslint.configs.recommendedTypeChecked,
+  //...tseslint.configs.recommended,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...tseslint.configs.recommendedTypeChecked,
+  
+  ...tseslint.configs.strictTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        project: ['./tsconfig.eslint.json', './engine/tsconfig.eslint.json', './engine/tsconfig.spec.json'],
+        project: ['./tsconfig.eslint.json', './engine/tsconfig.eslint.json', './tsconfig.spec.json', './engine/tsconfig.spec.json', './tsconfig.vite.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'error',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
+      //'@typescript-eslint/no-unsafe-argument': 'error',
+      //'@typescript-eslint/no-unsafe-assignment': 'error',
+      //'@typescript-eslint/no-unsafe-return': 'error',
     },
   },
-  prettierConfig,
+  {
+      files: ["**/*.spec.ts", "**/vite.config.ts"], // or any other pattern
+      plugins: {
+        vitest
+      },
+      rules: {
+        ...vitest.configs.recommended.rules,
+        //...vitest.configs.all.rules,
+        "vitest/max-nested-describe": ["error", { "max": 3 }],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+    }, 
+  },
+  
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**'],
+  },
+  {
+    files: ['tests/**'],
+    rules: {
+      // Customize Playwright rules
+      // ...
+    },
+  },
 );
--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   },
 
   plugins: [
-    tailwindcss(),
+    tailwindcss(), // eslint-disable-line @typescript-eslint/no-unsafe-call
     devServer({
       entry: 'src/server.ts',
       exclude: [
