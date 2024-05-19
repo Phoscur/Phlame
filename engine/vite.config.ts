e9b4f8db721013dcbbdfb32c58097831bfaaced0
Phoscur <phoscur@pheelgood.net>
2024-05-19 02:11:59 +0200

    Fix engine tests

--- engine/vite.config.ts
+++ engine/vite.config.ts
@@ -2,18 +2,17 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../node_modules/.vite/engine',
 
   plugins: [
-    nxViteTsPaths(),
+    tsconfigPaths(),
     dts({
       entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
   ],
 
