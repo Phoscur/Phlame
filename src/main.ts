25be91e6bd0953d944e328750ff15da492431ca0
Phoscur <phoscur@pheelgood.net>
2025-07-23 22:41:22 +0200

    Add DOMInjector (@joist/di v4)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import htmx from 'htmx.org';
+import { DOMInjector } from '@joist/di';
 import { AppElement } from './app/app.element';
-import { DebugCtx } from './app/debug.element';
+import { ConsoleDebug, Debug, DebugCtx } from './app/debug.element';
 import { GameContextElement } from './app/game.element';
 import { EmpireElement, PhlameElement } from './app/engine/empire.element';
 import { ClockElement, PercentElement, TickElement, ZeitContextElement } from './app/tick';
 import { PlanetElement } from './app/planet.element';
 import { ResourceElement, ResourcesElement } from './app/resources.element';
 import { LanguageSelectDropdownElement } from './app/language.dropdown.element';
+
+const app = new DOMInjector({ providers: [[Debug, { use: ConsoleDebug }]] });
+
+app.attach(document.body);
 
 customElements.define('debug-ctx', DebugCtx);
 customElements.define('app-root', AppElement);
