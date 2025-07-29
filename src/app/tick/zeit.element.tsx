5becf99712223c90d8f846e89722e082ed249754
Phoscur <phoscur@pheelgood.net>
2025-07-29 16:33:45 +0200

    Fix Zeitgeber needs to be stopped before reconfiguring
    - on disconnect is too late

--- src/app/tick/zeit.element.tsx
+++ src/app/tick/zeit.element.tsx
@@ -17,12 +17,12 @@ export class ZeitContextElement extends HTMLElement {
     const time = Number(this.attributes.getNamedItem('time')?.value) || Date.now();
     const tick = Number(this.attributes.getNamedItem('tick')?.value) || 0;
 
+    this.#zeit().stop();
     this.#zeit().start(time, tick);
     logger.log('Zeit Ctx connected, start!', time, tick);
   }
 
   disconnectedCallback() {
     this.#logger().log('Zeit Ctx disconnected, stop!');
-    this.#zeit().stop();
   }
 }
