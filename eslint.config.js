8af953336dd9d30908218f0451214a567d100e43
Phoscur <phoscur@pheelgood.net>
2026-02-21 22:22:02 +0100

    Update eslint to v10, drop plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,78 +1,10 @@
-// @ts-check
+import { defineConfig } from 'eslint/config';
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import vitest from 'eslint-plugin-vitest';
-import playwright from 'eslint-plugin-playwright';
-
-export default tseslint.config(
-  { ignores: ['playwright-report/*', 'assets/*', 'dist/*'] },
-
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  ...tseslint.configs.strictTypeChecked,
+export default defineConfig([
   {
     languageOptions: {
-      parserOptions: {
-        project: [
-          './tsconfig.eslint.json',
-          './engine/tsconfig.eslint.json',
-          './tsconfig.spec.json',
-          './engine/tsconfig.spec.json',
-          './tsconfig.vite.json',
-        ],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off', // we prefix those with an underscore and oxlint is okay with that
-      '@typescript-eslint/restrict-template-expressions': 'off', // I don't like it
-      '@typescript-eslint/no-confusing-void-expression': 'off',
-      '@typescript-eslint/no-extraneous-class': 'off', // no classes with only a constructor are legit -.-
+      ecmaVersion: 2015,
+      sourceType: 'module',
     },
   },
-  {
-    files: ['**/*.element.tsx'],
-    rules: {
-      '@typescript-eslint/no-non-null-assertion': 'off', // use with care (for querySelector)
-    },
-  },
-  {
-    files: ['**/*.spec.ts', '**/vite.config.ts'],
-    plugins: {
-      vitest,
-    },
-    settings: {
-      vitest: {
-        typecheck: true,
-      },
-    },
-    languageOptions: {
-      globals: {
-        ...vitest.environments.env.globals,
-      },
-    },
-    rules: {
-      ...vitest.configs.recommended.rules,
-      //...vitest.configs.all.rules,
-      'vitest/max-nested-describe': ['error', { max: 3 }],
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      'vitest/valid-expect': 'off', // TODO? refactor matchers?
-    },
-  },
-
-  {
-    ...playwright.configs['flat/recommended'],
-    files: ['tests/**'],
-  },
-  {
-    files: ['tests/**'],
-    rules: {
-      // Customize Playwright rules
-      // ...
-    },
-  },
-);
+]);
