6a9c77af840356ca19163cddf518ee8de1ee33ef
Phoscur <phoscur@pheelgood.net>
2026-02-21 21:35:31 +0100

    Add update while action

--- engine/src/lib/Action.ts
+++ engine/src/lib/Action.ts
@@ -1,3 +1,4 @@
+import { BuildingIdentifier } from './Building';
 import { TimeUnit } from './resources';
 
 // it is a bit unwise to loosen the identifier type to string or number
@@ -8,17 +9,82 @@ export interface Entity {
   id: ID;
 }
 
-// TODO use `as const` instead of enum
-export enum ActionTypes {
-  CREATE = 'create',
-  UPDATE = 'update',
-}
-export default interface Action<Type extends ActionTypes> {
+export const ActionTypes = {
+  CREATE: 'create',
+  UPDATE: 'update',
+  DESTROY: 'destroy',
+} as const;
+
+export type ActionType = (typeof ActionTypes)[keyof typeof ActionTypes];
+/**
+ * basic ActionJSON
+ */
+export interface Action<Type extends ActionType> {
   type: Type;
   concerns: Entity;
+  // TODO! multi consequences? just queue additional actions...?!
   consequence: {
     at: TimeUnit; // ticks
     type: Type; // event trigger
     payload: Record<string | number | symbol, unknown>;
   };
+}
+
+export class ActionFactory {
+  create<Type extends ActionType>(
+    type: Type,
+    at: TimeUnit,
+    concerns: Entity,
+    payload: Record<string | number | symbol, unknown>,
+  ): Action<Type> {
+    return {
+      type,
+      concerns,
+      consequence: {
+        at,
+        type,
+        payload,
+      },
+    };
+  }
+
+  createBuilding(at: TimeUnit, concerns: Entity, buildingID: BuildingIdentifier) {
+    return this.create(ActionTypes.CREATE, at, concerns, { buildingID });
+  }
+  updateBuilding(
+    at: TimeUnit,
+    concerns: Entity,
+    buildingID: BuildingIdentifier,
+    grade: 'up' | 'down',
+  ) {
+    return this.create(ActionTypes.UPDATE, at, concerns, { buildingID, grade });
+  }
+}
+
+export const EventTypes = {
+  ACTION: 'action',
+  CONSEQUENCE: 'consequence',
+  SYSTEM: 'system',
+} as const;
+
+export type EventType = (typeof EventTypes)[keyof typeof EventTypes];
+export interface Event {
+  at: TimeUnit;
+  type: EventType;
+  concerns: ID;
+  action: Action<ActionType>;
+}
+
+export class EventFactory {
+  create(type: EventType, at: TimeUnit, concerns: ID, action: Action<ActionType>): Event {
+    return {
+      at,
+      type,
+      concerns,
+      action,
+    };
+  }
+  fromAction(at: TimeUnit, action: Action<ActionType>): Event {
+    return this.create(EventTypes.ACTION, at, action.concerns.id, action);
+  }
 }
--- engine/src/lib/Phlame.spec.ts
+++ engine/src/lib/Phlame.spec.ts
@@ -1,4 +1,4 @@
-import Action, { ActionTypes } from './Action';
+import { Action, ActionType, ActionTypes } from './Action';
 
 import examples, { Types } from './resources/examples';
 import { stock, buildings, phlame } from './examples';
@@ -40,37 +40,76 @@ describe('Phlame Entity', () => {
     );
   });
 
-  it('should queue actions', () => {
-    const eco = new Economy('Eco', stock, buildings);
-    const phlame = new Phlame('InAction', eco);
-    expect(phlame.toString()).to.eql(
-      'InAction (Processing energy&resources: 30/50 energy, 0/0 heat, 3salties(0, Infinity): +20, 15blubbs(0, Infinity): 0, 3tumbles(0, Infinity): 0) Building(12, 1, 100%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 1, 100%)',
-    );
-    const action: Action<ActionTypes> = {
-      type: ActionTypes.CREATE,
-      concerns: phlame,
-      consequence: {
-        type: ActionTypes.UPDATE,
-        at: 15,
-        payload: {
-          id: 1,
-          level: 2,
-        },
-      },
-    };
-    phlame.add(action);
-    expect(phlame.recent).to.contain(action);
-  });
-
-  // TODO? it("should queue a maximum of a type of actions");
-
   it('should update (in) time: ticks', () => {
     expect(phlame.lastTick).to.eql(0);
 
     phlame.update(1);
 
     expect(phlame.lastTick).to.eql(1);
+
+    phlame.update(2);
+
+    expect(phlame.lastTick).to.eql(2);
   });
+
+  it.only('should queue and execute actions', () => {
+    const eco = new Economy('Eco', stock, buildings);
+    const phlame = new Phlame('InAction', eco);
+    expect(phlame.toString()).to.eql(
+      'InAction (Processing energy&resources: 30/50 energy, 0/0 heat, 3salties(0, Infinity): +20, 15blubbs(0, Infinity): 0, 3tumbles(0, Infinity): 0) Building(12, 1, 100%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 1, 100%)',
+    );
+    const action: Action<ActionType> = {
+      type: ActionTypes.CREATE,
+      concerns: phlame,
+      consequence: {
+        type: ActionTypes.UPDATE,
+        at: 15,
+        payload: {
+          buildingID: 2,
+          grade: 'up',
+        },
+      },
+    };
+    phlame.add(action);
+    expect(phlame.recent).to.contain(action);
+    phlame.update(15);
+    // at time of the consequence
+    expect(phlame.lastTick).to.eql(15);
+    // TODO?! clean recent expect(phlame.recent).to.be.empty;
+    expect(phlame.toString()).to.eql(
+      'InAction (Processing energy&resources: -3/100 energy, 0/0 heat, Degraded to 94%, 303salties(0, Infinity): +81, 15blubbs(0, Infinity): -1, 3tumbles(0, Infinity): 0) ' +
+        'Building(12, 1, 100%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 2, 100%)',
+    );
+
+    // TODO investigate, why do we have /100 energy now? (should be /50)?
+    // expect(phlame.productionTable[0]).to.eql(['energy', -3, 50]);
+
+    // a few ticks later
+    phlame.update(20);
+    expect(phlame.lastTick).to.eql(20);
+    expect(phlame.toString()).to.eql(
+      'InAction (Processing energy&resources: -3/100 energy, 0/0 heat, Degraded to 94%, 708salties(0, Infinity): +81, 10blubbs(0, Infinity): -1, 3tumbles(0, Infinity): 0) ' +
+        'Building(12, 1, 100%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 2, 100%)',
+    );
+
+    // uh oh - out of energy
+    phlame.update(30);
+    expect(phlame.lastTick).to.eql(30);
+    expect(phlame.toString()).to.eql(
+      'InAction (Processing energy&resources: -3/100 energy, 0/0 heat, Degraded to 94%, 1518salties(0, Infinity): +81, 0blubbs(0, Infinity): -1, 3tumbles(0, Infinity): 0) ' +
+        'Building(12, 1, 100%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 2, 100%)',
+    );
+
+    // halting now
+    phlame.update(31);
+    expect(phlame.lastTick).to.eql(31);
+    expect(phlame.toString()).to.eql(
+      'InAction (Processing energy&resources: -53/0 energy, 0/0 heat, Degraded to 0%, 1518salties(0, Infinity): 0, 0blubbs(0, Infinity): 0, 3tumbles(0, Infinity): 0) ' +
+        'Building(12, 1, 0%), Building(3, 1, 100%), Building(0, 1, 50%), Building(2, 2, 100%)',
+    );
+  });
+
+  // TODO? it("should queue a maximum of a type of actions");
 
   it.todo(
     'should have env properties - e.g. base temperature and amounts of resources to start from',
